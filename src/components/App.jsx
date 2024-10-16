e4d260b40cee13cb83629ba95115ea1029f6e74e
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-10-16 13:15:48 -0700

    more refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+// Core Deliverables: Import React, components, and PlantProvider
 import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
-import { PlantProvider } from "./PlantContext"; // Import the provider
+import { PlantProvider } from "./PlantContext"; // Import the context provider
 
-// Core Deliverables
-// Wrap the app with PlantProvider
+// Core Deliverables: Wrap the app with PlantProvider to give access to context
 function App() {
   return (
     <PlantProvider> {/* Core Deliverables */}
       <div className="app">
-        <Header />
-        <PlantPage />
+        <Header /> {/* Core Deliverables */}
+        <PlantPage /> {/* Core Deliverables */}
       </div>
     </PlantProvider>
   );
